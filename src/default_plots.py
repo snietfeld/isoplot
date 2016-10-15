50f8110bf72d31d5a062dda4e776d1f693e43316
Scott Nietfeld <snietfeld@masten.aero>
2016-10-15 12:49:48 -0700

    Added default plot module and started implementing user module importing.

---
+++ src/default_plots.py
@@ -0,0 +1,18 @@
+
+#--------------------------------------------------------------------------------
+# isoplot: default_plots.py
+#
+# Description: A set of default plot functions for basic analysis. Also serves
+#              as an example for users to create their own plotting modules.
+#--------------------------------------------------------------------------------
+
+
+class Derp:
+    def __init__(self):
+        print("Init!")
+
+def scatter(fname, datadict):
+    print("Scatter!")
+
+def line(fname, datadict):
+    print("Line!")
--- src/isoplot.py
+++ src/isoplot.py
@@ -1,1 +1,53 @@
-# pyuic4 isoplot_gui.ui -o isoplot_gui.py
+
+#--------------------------------------------------------------------------------
+# isoplot - A lightweight data plotting and analysis application
+#
+# Author: Scott Nietfeld
+# Initial Commit: 2016-10-12
+#
+# Compiling the GUI:  pyuic4 isoplot_gui.ui -o isoplot_gui.py
+#--------------------------------------------------------------------------------
+
+import os
+
+
+
+def load_module(path):
+    import imp
+    
+    print("Loading module from %s" % path)
+    full_path = os.path.abspath(path)
+    base_name = os.path.basename(full_path)
+    mod_name= base_name.split('.')[0]
+
+    print("Full path: %s" % full_path)
+    print("Base name: %s" % base_name)
+    print("Module name: %s" % mod_name)
+    
+    mod = imp.load_source(base_name, full_path)
+    return mod
+
+def get_functions(mod):
+    import inspect
+
+    all_functions = inspect.getmembers(mod, inspect.isfunction)
+    return all_functions
+
+def unload_module(path):
+    pass
+
+def load_data_file(path):
+    pass
+
+def run_usr_function(path):
+    pass
+
+
+if __name__=="__main__":
+    print("Hello.")
+
+    mod = load_module("./default_plots.py")
+    f_list = get_functions(mod)
+    
+    print f_list
+    
